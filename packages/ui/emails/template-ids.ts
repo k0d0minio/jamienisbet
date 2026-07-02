5867767877d3ff49a16edd97b35c4402b54f438b
Jamie Nisbet <jamie.nisbet@outlook.be>
2026-07-02 11:09:29 +0100

    template ids

--- packages/ui/emails/template-ids.ts
+++ packages/ui/emails/template-ids.ts
@@ -2,6 +2,6 @@
 // so they're checked in here instead of duplicated as an env var per site/template.
 // Publish a template (see README.md), then paste its ID below.
 export const EMAIL_TEMPLATE_IDS = {
-  contactFormNotification: "",
-  referralLeadNotification: "",
+  contactFormNotification: "c86c107f-7e73-42e3-ac93-16c6ef18632e",
+  referralLeadNotification: "256d049d-bc87-4267-9e91-cbe80b3ca324",
 } as const
